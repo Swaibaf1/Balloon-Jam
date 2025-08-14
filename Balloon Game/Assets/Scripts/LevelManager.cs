c72a1898b6251c20f17a8f025854a363faf895b3
ratbagvalley <107149514+ratbagvalley@users.noreply.github.com>
2025-08-14 16:37:08 +0100

    Spawning
    Threats now spawn choosing from one of 7 random possible positions. There is also a min and max amount of spawns allowed to happen at one time with adjustable interval. Using a hash set to create a unique identifier for each of the spawned threats to make sure no spawns can choose the same spot.

--- Balloon Game/Assets/Scripts/LevelManager.cs
+++ Balloon Game/Assets/Scripts/LevelManager.cs
@@ -4,15 +4,100 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] DynamicObjectConfig DefaultTestSpawn;
+    [SerializeField] DynamicObjectConfig FabricSpawn;
+    [SerializeField] DynamicObjectConfig ThreatSpawn;
     [SerializeField] List<GameObject> SpawnPositions = new List<GameObject>();
+
+    [SerializeField] HashSet<int> usedPositions = new HashSet<int>();
+
+
+    public float spawnTimer;
+    public int spawnInterval = 5;
+    public int minSpawnCount = 1;
+    public int maxSpawnCount = 3;
+
 
     private void Start()
     {
-        DynamicObject newObject = DynamicObject.CreateDynamicObject(DefaultTestSpawn);
+        
+    }
 
-        GameObject randPosition = SpawnPositions[Random.Range(0, SpawnPositions.Count)];
-        newObject.transform.position = randPosition.transform.position;
+    private void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= spawnInterval)
+        {
+            //SpawnThreat();
+
+            SpawnThreatNew();
+        }
+
+    }
+
+    private void SpawnFabric()
+    {
+        for (int i = 0; i < SpawnPositions.Count; i++)
+        {
+            DynamicObject newObject = DynamicObject.CreateDynamicObject(FabricSpawn);
+
+            GameObject randPosition = SpawnPositions[Random.Range(0, SpawnPositions.Count)];
+            newObject.transform.position = randPosition.transform.position;
+
+            spawnTimer = 0;
+        }
+    }
+
+    private void SpawnThreat()
+    {
+        int spawnCount = Random.Range(minSpawnCount, maxSpawnCount + 1);
+        
+        for (int i = 0; i < SpawnPositions.Count; i++)
+        {
+            DynamicObject newObject = DynamicObject.CreateDynamicObject(ThreatSpawn);
+
+            GameObject randPosition = SpawnPositions[Random.Range(0, SpawnPositions.Count)];
+            newObject.transform.position = randPosition.transform.position;
+
+            spawnTimer = 0;
+        }
+        
+    }
+
+    private void SpawnThreatNew()
+    {
+        if(SpawnPositions.Count <= 0)
+        {
+            return;
+        }
+
+        int spawnCount = Random.Range(minSpawnCount, maxSpawnCount + 1);
+        for(int i = 0; i < spawnCount; i++)
+        {
+            int randomIndex;
+
+
+            for (int y = 0; y < 10; y++)
+            {
+                randomIndex = Random.Range(0, SpawnPositions.Count);
+
+                if(!usedPositions.Contains(randomIndex))
+                {
+                    usedPositions.Add(randomIndex);
+                    break;
+                }
+
+            }
+
+            DynamicObject newObject = DynamicObject.CreateDynamicObject(ThreatSpawn);
+
+            GameObject randPosition = SpawnPositions[Random.Range(0, SpawnPositions.Count)];
+            newObject.transform.position = randPosition.transform.position;
+
+            spawnTimer = 0;
+        }
+
+        
     }
 
 
